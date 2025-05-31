06a98fbaa3f2f2ab7ad71146f6edbf8d71bf137f
turcid <turcid@protonmail.com>
2025-05-31 18:53:13 -0500

    Alerts are now sorted by severity! Note the sort is by what I deem to be important

--- src/Include/weather.py
+++ src/Include/weather.py
@@ -17,6 +17,33 @@ class WxAlert: #Using a class will make extending functionality easier later
         self.event = event
         self.area = area
         self.desc = desc
+        self.mark = 0
+        self.setMark()
+
+    def setMark(self): #Assign a mark to the alert to determine importance so the list can be sorted
+        if 'Emergency' in self.event and 'Tornado' in self.event:
+            self.mark = 10
+
+        elif 'Warning' in self.event and 'Tornado' in self.event:
+            self.mark = 9
+
+        elif 'Warning' in self.event and 'Thunderstorm' in self.event:
+            self.mark = 8
+        
+        elif 'Special Weather Statement' in self.event:
+            self.mark = 7
+        
+        elif 'Watch' in self.event and 'Tornado' in self.event:
+            self.mark = 6
+
+        elif 'Watch' in self.event and 'Thunderstorm' in self.event:
+            self.mark = 5
+
+        elif 'Warning' in self.event and 'Flood' in self.event:
+            self.mark = 4
+
+        elif 'Flood' in self.event and 'Watch' in self.event:
+            self.mark = 3
 
 
 #Functions
@@ -33,7 +60,8 @@ def parseAlerts(content): # parse the values we want from the api response
 
                 nAlert = WxAlert(alert['properties']['event'], alert['properties']['areaDesc'], alert['properties']['parameters']['NWSheadline'][0]) 
                 alertList.append(nAlert)
-
+        
+        alertList.sort(key=lambda x: x.mark, reverse=True)
         return alertList
 
     except IndexError: # If no alerts, an Index error will be raised
