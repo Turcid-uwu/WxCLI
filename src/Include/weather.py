d589fe4d7e94379aaa93a21578bddc463e52cda7
turcid <turcid@protonmail.com>
2025-05-31 16:27:25 -0500

    Fixed some readabilty issues and added more comments

--- src/Include/weather.py
+++ src/Include/weather.py
@@ -1,10 +1,16 @@
 
 #Constants
-URL = "https://api.weather.gov/alerts/active?area=TX" #api endpoint for active weather alerts
+
 # use api.weather.gov/alerts/active?area={your state} for state wide
 # use api.weather.gov/alerts/active/zone/{your NWS county code} for county wide alerts
+
+URL = "https://api.weather.gov/alerts/active?area=TX" #api endpoint for active weather alerts
+
 NWS_OFFICE = "NWS Fort Worth TX" # Local forcast office, make sure this is set to your forcast office
+
 HEADERS = "" # Custom headers, If you want them 
+
+#Classes
 
 class WxAlert: #Using a class will make extending functionality easier later
     def __init__(self, event, area, desc):
@@ -13,7 +19,9 @@ class WxAlert: #Using a class will make extending functionality easier later
         self.desc = desc
 
 
-def parseAlerts(content):
+#Functions
+
+def parseAlerts(content): # parse the values we want from the api response
     try:
 
         test = content['features'][0]['properties']['event'] # Try to access the first element to see if there is any active alerts
@@ -21,7 +29,7 @@ def parseAlerts(content):
 
         for alert in content['features']: 
 
-            if alert['properties']['senderName'] == NWS_OFFICE:
+            if alert['properties']['senderName'] == NWS_OFFICE: # Not really need if going by county, but still helpful to have
 
                 nAlert = WxAlert(alert['properties']['event'], alert['properties']['areaDesc'], alert['properties']['parameters']['NWSheadline'][0]) 
                 alertList.append(nAlert)
