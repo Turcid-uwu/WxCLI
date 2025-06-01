3a3d53e592efcaf6fc7f7ce8f26f361cb9dcae1e
Turcid <61513903+cursed-bot@users.noreply.github.com>
2025-06-01 13:03:46 -0500

    add a lil extra to that comment

--- src/Include/weather.py
+++ src/Include/weather.py
@@ -56,7 +56,7 @@ def parseAlerts(content): # parse the values we want from the api response
 
         for alert in content['features']: 
 
-            if alert['properties']['senderName'] == NWS_OFFICE: # Not really needed if going by county, but still helpful to have
+            if alert['properties']['senderName'] == NWS_OFFICE: # Not really needed if going by county/zone, but still helpful to have
 
                 nAlert = WxAlert(alert['properties']['event'], alert['properties']['areaDesc'], alert['properties']['headline']) 
                 alertList.append(nAlert)
