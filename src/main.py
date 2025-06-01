9b15aaff7e20cf383435eba639b237258d426703
turcid <turcid@protonmail.com>
2025-06-01 15:31:43 -0500

    Added Windows compat

--- src/main.py
+++ src/main.py
@@ -7,9 +7,16 @@ from Include.weather import *
 from Include.parse import *
 
 def main():
+
+    # Get OS name
+    osName = os.name
+    if osName == 'posix':
+        CLEARCMD = 'clear'
+    else:
+        CLEARCMD = 'cls'
   
     while True:
-        os.system('clear')
+        os.system(CLEARCMD)
         req = requests.get(URL, headers=HEADERS)
         rawResponse = json.loads(req.content)
         alerts = parseAlerts(rawResponse)
@@ -25,12 +32,12 @@ def main():
                 sleep(2)
             print('*---------------------------------------------------*')
             sleep(60)
-            os.system('clear')
+            os.system(CLEARCMD)
 
         else:
             print('No Alerts for your area are currently issued')
             sleep(60)
-            os.system('clear')
+            os.system(CLEARCMD)
 
 if __name__ == '__main__':
     main()
